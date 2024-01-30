7e7945f85886921761b7100bafeaee15202d9152
Toshiki Ito <ito@monoworks.co.jp>
2024-01-31 08:19:56 +0900

    first package

---
+++ dist/components/Text/Text.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+export type TextProps = {
+    children: React.ReactNode;
+};
+export declare function Text({ children }: TextProps): import("react/jsx-runtime").JSX.Element;
