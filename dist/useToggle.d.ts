e2ea51acd7e10743923023a3a128c437f4ae5c45
Toshiki Ito <ito@monoworks.co.jp>
2024-02-04 09:41:30 +0900

    dist

--- dist/useToggle.d.ts
+++ dist/useToggle.d.ts
@@ -1,2 +1,5 @@
-declare const useToggle: (initialValue?: boolean) => [boolean, () => void];
+/**
+ * A quick and simple utility for toggle states
+ */
+declare const useToggle: (initialState?: boolean) => [boolean, () => void];
 export default useToggle;
